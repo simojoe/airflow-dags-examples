8f9a14d4618e163d768ca1bf80c39da0abef063f
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-01-08 16:11:53 -0500

    update operator path sicsic

--- hello-world-state.py
+++ hello-world-state.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.providers.http import SimpleHttpOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
 default_args = {
     "owner": "airflow",
